eab11ddc82e76a914a6998536146e897d4431e5e
Jeanetted3v <jeanetted3v@gmail.com>
2021-09-29 20:34:41 +0800

    - refactored and renamed Content View to HomeView - organise files into folders - add ContentModel - set ContentModel as EnvironmentObject

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Zhao Qianyu on 29/9/21.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject, Identifiable {
+    
+    
+}
